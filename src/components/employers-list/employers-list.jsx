8ab48eacf90ca661e4c0b53fa60f4ad99453850f
Vasiliy <63386773+helllrice@users.noreply.github.com>
2022-05-01 21:45:19 +0300

    изменение стате

--- src/components/employers-list/employers-list.jsx
+++ src/components/employers-list/employers-list.jsx
@@ -1,12 +1,15 @@
 import './employers-list.css';
 import EmployersListItem from '../employers-list-item/employers-list-item';
 
-const EmployersList = ({data}) => {
+const EmployersList = ({data, onDelete}) => {
     
     const elements = data.map(item => {
         const {id, ...itemProps} = item;
         return (
-            <EmployersListItem key={id} {...itemProps}/>
+            <EmployersListItem 
+            key={id} 
+            {...itemProps} 
+            onDelete={() => onDelete(id)}/>
         )
     })
     
